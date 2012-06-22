977d0d5b0ba3cbca37bb476d1372eed7958c0172
Jeff Anderson <jefferya@programmerq.net>
2012-06-21 18:01:56 -0600

    switched runtests to use django-nose

--- tagging/tests/runtests.py
+++ tagging/tests/runtests.py
@@ -1,29 +1,33 @@
 #!/usr/bin/env python
 
-import os
-from django.utils import unittest
+"""Boilerplate runtests.py for using django-nose to test this app. Adapted from
+dcramer's post on https://github.com/jbalogh/django-nose/issues/46"""
 
-if not 'DJANGO_SETTINGS_MODULE' in os.environ:
-    os.environ['DJANGO_SETTINGS_MODULE'] = 'tagging.tests.settings'
+import sys
+import os, os.path
+from optparse import OptionParser
 
 from django.conf import settings
-from django.db import connection
-from django.test.utils import setup_test_environment
-from django.test.utils import teardown_test_environment
-from django.utils.importlib import import_module
 
-TEST_MODULES = ["tagging.tests.tests"]
+if 'DJANGO_SETTINGS_MODULE' not in os.environ:
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'tagging.tests.settings'
 
-def run_tests():
-    setup_test_environment()
-    db_name = settings.DATABASE_NAME
-    suite = unittest.TestSuite()
-    for module_name in TEST_MODULES:
-        module = import_module(module_name)
-        suite.addTest(unittest.defaultTestLoader.loadTestsFromModule(module))
-    connection.creation.create_test_db()
-    unittest.TextTestRunner().run(suite)
-    teardown_test_environment()
+from django_nose import NoseTestSuiteRunner
 
-if __name__ == "__main__":
-    run_tests()
+def runtests(*test_args, **kwargs):
+    if 'south' in settings.INSTALLED_APPS:
+        from south.management.commands import patch_for_test_db_setup
+        patch_for_test_db_setup()
+
+    test_runner = NoseTestSuiteRunner(**kwargs)
+
+    failures = test_runner.run_tests(test_args)
+    sys.exit(failures)
+
+if __name__ == '__main__':
+    parser = OptionParser()
+    parser.add_option('--verbosity', dest='verbosity', action='store', default=1, type=int)
+    parser.add_options(NoseTestSuiteRunner.options)
+    (options, args) = parser.parse_args()
+
+    runtests(*args, **options.__dict__)
